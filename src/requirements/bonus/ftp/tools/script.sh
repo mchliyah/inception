3f7f0729702bf095df13ac7afb2c7f697998f9ac
mchliyah <mchliyah@mchliyah.42.fr>
2023-02-23 06:53:18 +0100

    fixing errors

--- src/requirements/bonus/ftp/tools/script.sh
+++ src/requirements/bonus/ftp/tools/script.sh
@@ -1,26 +1,12 @@
-#!/bin/bash
-
-service vsftpd start
-
-# Add the USER, change his password and declare him as the owner of wordpress folder and all subfolders
-
-adduser $FTP_USER --disabled-password
-
-echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
-
-#Add the FTP user to the list of allowed users in the vsftpd configuration file /etc/vsftpd.userlist using the tee command.
-
-echo "$FTP_USER" | tee -a /etc/vsftpd.userlist 
-
-
-mkdir /home/$FTP_USER/ftp
-
-
+#!/bin/sh
+useradd -m $FTP_USER
+echo $FTP_USER:$FTP_PASS | chpasswd
+echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
+mkdir -p /home/$FTP_USER/dir
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/dir
 
-service vsftpd stop
-
-/usr/sbin/vsftpd
+vsftpd
 
 # service vsftpd start
 
