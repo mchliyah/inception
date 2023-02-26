baed8c25e5243853af4f8cf1563d71bb387588cf
mchliyah <mchliyah@mchliyah.42.fr>
2023-02-26 04:19:27 +0100

    redis is done

--- src/requirements/bonus/ftp/tools/script.sh
+++ src/requirements/bonus/ftp/tools/script.sh
@@ -3,8 +3,8 @@ adduser --disabled-password --gecos '' $FTP_USER
 echo $FTP_USER:$FTP_PASS | chpasswd
 mkdir -p /home/$FTP_USER/ftp
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
-mkdir -p /home/$FTP_USER/ftp/files
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
+mkdir -p /home/$FTP_USER/empty
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/empty
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
 vsftpd
--- src/requirements/wordpress/tools/start.sh
+++ src/requirements/wordpress/tools/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+chown -R www-data:www-data /var/www/html
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
 	&& apt clean && rm -rf /var/lib/apt/lists/*
@@ -19,6 +20,20 @@ wp core install --url=$DOMAIN_NAME \
 --admin_password=$ADMIN_PASS \
 --admin_email=$ADMIN_EMAIL \
 --allow-root
+
+
+wp plugin install redis-cache --activate --allow-root
+
+wp plugin update --all --allow-root
+
+wp theme install twentytwentytwo --activate --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+wp config set WP_REDIS_HOST redis --allow-root
+
+wp redis enable --allow-root
 
 # mv wp-config-sample.php wp-config.php
 exec "$@"
